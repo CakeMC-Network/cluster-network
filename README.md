28767ad104d45165c2fc7d170999201bdead77d8
u64lisa <Nils-Schrock@web.de>
2024-10-14 15:34:30 +0200

    :sparkles: updated readme

---
+++ README.md
@@ -0,0 +1,74 @@
+# Cluster-Network
+
+Creating a cluster-node:
+````java
+
+package test;
+
+import net.cakemc.library.cluster.SynchronisationType;
+import net.cakemc.library.cluster.api.Authentication;
+import net.cakemc.library.cluster.api.ClusterContext;
+import net.cakemc.library.cluster.api.MemberIdentifier;
+import net.cakemc.library.cluster.api.SoftPublication;
+
+public class Example {
+
+  public static void main(String[] args) {
+    // creating authentication "config"
+    Authentication authentication = Authentication
+            .make()
+            .useVerification()
+            .withKey("my-key")
+            .get();
+
+    // creating a cluster Context
+    ClusterContext context = ClusterContext
+            // own node id
+            .make(1)
+            // registering a outgoing channel
+            .register("test")
+            // registering the publication type
+            .withPublicationType(SoftPublication.class)
+            // cluster type
+            .type(SynchronisationType.RING)
+            // prioritised nodes
+            .priorities(1, 2, 3)
+            // own information
+            .identifier(1, "127.0.0.1", 7001)
+            // cluster infrastructure
+            .members(
+                    MemberIdentifier.of(1, "127.0.0.1", 7001),
+                    MemberIdentifier.of(2, "127.0.0.1", 7002),
+                    MemberIdentifier.of(3, "127.0.0.1", 7003)
+            )
+            // authing inside the cluster
+            .authentication(authentication)
+            .get();
+
+    // subscribing to a channel and listening to publications
+    context.subscribe("test", (session, message, withNodes, out) -> {
+      // incoming publications of other nodes to this channel
+      return true;
+    });
+
+    // creating a publisher 
+    context.publisher()
+            // skipping own id
+            .skip(1)
+            // sending type
+            .synchronisationType(SynchronisationType.UNI_CAST)
+            // returning result
+            .callBack((session, message, withNodes, out) -> {
+              // will be called on a result
+              return true;
+            })
+            // sending a publication
+            .release(new SoftPublication(
+                    1l, 1l, 1,
+                    "test", new byte[]{ 1, 3, 3, 7 },
+                    1
+            ));
+  }
+
+}
+````
--- src/main/java/test/Example.java
+++ src/main/java/test/Example.java
@@ -1,6 +1,7 @@
+
 package test;
 
-import net.cakemc.library.cluster.*;
+import net.cakemc.library.cluster.SynchronisationType;
 import net.cakemc.library.cluster.api.Authentication;
 import net.cakemc.library.cluster.api.ClusterContext;
 import net.cakemc.library.cluster.api.MemberIdentifier;
@@ -9,44 +10,60 @@ import net.cakemc.library.cluster.api.SoftPublication;
 public class Example {
 
 	public static void main(String[] args) {
+		// creating authentication "config"
 		Authentication authentication = Authentication
 			 .make()
 			 .useVerification()
 			 .withKey("my-key")
 			 .get();
 
+		// creating a cluster Context
 		ClusterContext context = ClusterContext
+			 // own node id
 			 .make(1)
+			 // registering a outgoing channel
 			 .register("test")
+			 // registering the publication type
 			 .withPublicationType(SoftPublication.class)
+			 // cluster type
 			 .type(SynchronisationType.RING)
+			 // prioritised nodes
 			 .priorities(1, 2, 3)
+			 // own information
 			 .identifier(1, "127.0.0.1", 7001)
+			 // cluster infrastructure
 			 .members(
-				   MemberIdentifier.of(1, "127.0.0.1", 7001),
-				   MemberIdentifier.of(2, "127.0.0.1", 7002),
-				   MemberIdentifier.of(3, "127.0.0.1", 7003)
+				  MemberIdentifier.of(1, "127.0.0.1", 7001),
+				  MemberIdentifier.of(2, "127.0.0.1", 7002),
+				  MemberIdentifier.of(3, "127.0.0.1", 7003)
 			 )
+			 // authing inside the cluster
 			 .authentication(authentication)
 			 .get();
 
+		// subscribing to a channel and listening to publications
 		context.subscribe("test", (session, message, withNodes, out) -> {
 
 			return true;
 		});
 
+		// creating a publisher
 		context.publisher()
-			 .skip(1)
-			 .synchronisationType(SynchronisationType.UNI_CAST)
-			 .callBack((session, message, withNodes, out) -> {
-
-				 return true;
-			 })
-			 .release(new SoftPublication(
-				  1l, 1l, 1,
-				  "test", new byte[] { 1, 3, 3, 7 },
-				  1
-			 ));
+		       // skipping own id
+		       .skip(1)
+		       // sending type
+		       .synchronisationType(SynchronisationType.UNI_CAST)
+		       // returning result
+		       .callBack((session, message, withNodes, out) -> {
+			       // will be called on a result
+			       return true;
+		       })
+		       // sending a publication
+		       .release(new SoftPublication(
+			        1l, 1l, 1,
+			        "test", new byte[]{ 1, 3, 3, 7 },
+			        1
+		       ));
 	}
 
 }
